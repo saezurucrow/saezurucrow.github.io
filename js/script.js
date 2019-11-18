69c18863017fb37bcd5bf539d68e30fcf6cbe85c
saezurucrow <usobukuhito@gmail.com>
2019-11-18 22:11:48 +0900

    vatは悪

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // モーダル表示 https://webdesignday.jp/inspiration/pickup/4680/
 
-$(function() {
-  $('.js-modal-open').each(function() {
-    $(this).on('click', function() {
-      var target = $(this).data('target')
-      var modal = document.getElementById(target)
+$(function () {
+  $('.js-modal-open').each(function () {
+    $(this).on('click', function () {
+      let target = $(this).data('target')
+      let modal = document.getElementById(target)
       $(modal).fadeIn()
       return false
     })
   })
-  $('.js-modal-close').on('click', function() {
+  $('.js-modal-close').on('click', function () {
     $('.js-modal').fadeOut()
     return false
   })
